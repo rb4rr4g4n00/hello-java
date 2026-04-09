408fd65bab6bcd898897f4b572fef23b9e1c6886
CARLITOX2000 <barragancarlosjesus@gmail.com>
2026-04-09 10:58:00 -0600

    guardar cambios

--- basic/c00_helloworld/HelloWorldExercises.java
+++ basic/c00_helloworld/HelloWorldExercises.java
@@ -1,4 +1,3 @@
-package basic.c00_helloworld;
 
 /*
 Clase 14 - Ejercicios: Primeros pasos
@@ -10,21 +9,38 @@ public class HelloWorldExercises {
     public static void main(String[] args) {
 
         // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
-
+        System.out.println("Hola, Soy Carlos.");
         // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
-
+        System.out.println("Hola\nMundo");
         // 3. Añade un comentario sobre lo que hace cada línea del programa.
-
+        System.out.println("Este programa muestra mensajes en pantalla.");
         // 4. Crea un comentario en varias líneas.
-
+  // 4. Crea un comentario en varias líneas.
+        /*
+         Hola soy Carlos
+         ¿Como estas?.
+         Sirve para explicar código más largo.
+        */
         // 5. Imprime tu edad, tu color favorito y tu ciudad.
-
+        System.out.println("Edad: 25");
+        System.out.println("Color favorito: Rojo y Nranja");
+        System.out.println("Ciudad: Ciudad de Mexico");
         // 6. Explora los diferentes System.XXX.println(); más allá de "out".
-
+        System.out.println("Este es un mensaje de error.");
+        System.out.println("Este es un mensaje normal.");
         // 7. Utiliza varios println para imprimir una frase.
-
+        System.out.println("Java");
+        System.out.println("es");
+        System.out.println("muy");
+        System.out.println("bueno.");
         // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
-
+        System.out.println("  *****  ");
+        System.out.println(" *     * ");
+        System.out.println("*  0 0  *");
+        System.out.println("*   ^   *");
+        System.out.println("* \\___/ *");
+        System.out.println(" *     * ");
+        System.out.println("  *****  ");
         // 9. Intenta ejecutar el programa sin el método main y observa el error.
 
         //10. Intenta cambiar el nombre del archivo a uno diferente del de la clase y compílalo. ¿Qué pasa?
