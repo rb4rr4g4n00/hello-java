375483322b15cec2b41b576bd117a53f64bdd73c
CARLITOX2000 <barragancarlosjesus@gmail.com>
2026-04-20 20:14:07 -0600

    solucion de los 4 primeros ejercicios

--- basic/c04_conditionals/ConditionalsExercises.java
+++ basic/c04_conditionals/ConditionalsExercises.java
@@ -1,4 +1,3 @@
-package basic.c04_conditionals;
 
 /*
 Clase 38 - Ejercicios: Condicionales
@@ -10,19 +9,58 @@ public class ConditionalsExercises {
     public static void main(String[] args) {
 
         // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
-
+        int edad = 20;
+        if (edad >= 18){
+            System.out.println("Puede votar");
+        }else {
+            System.out.println("No puede votar");
+        }
         // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
-
+        int num1 = 10;
+        int num2 = 20;
+        if (num1 > num2){
+            System.out.println("El mayor es: " + num1);
+        }else if (num2 > num1) {
+            System.out.println("El mayor es: " + num2);
+        } else {
+            System.out.println("Son iguales");
+        }
         // 3. Dado un número, verifica si es positivo, negativo o cero.
-
+        int numero = -5;
+        if (numero > 0) {
+            System.out.println("Es positivo");
+        } else if (numero < 0) {
+            System.out.println("Es negativo");
+        } else {
+            System.out.println("Es cero");
+        }
         // 4. Crea un programa que diga si un número es par o impar.
-
+        int n = 8;
+        if (n % 2 == 0) {
+            System.out.println("Es par");
+        } else {
+            System.out.println("Es impar");
+        }
         // 5. Verifica si un número está en el rango de 1 a 100.
 
         // 6. Declara una variable con el día de la semana (1-7) y muestra su nombre con switch.
 
         // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o "Suspenso" según la nota (0-100).
-
+        // Sobresaliente
+        if (calificacionAlumno >= 90 && calificacionAlumno <= 100) {
+            System.out.println("El alumno es sobresaliente");
+        } 
+        // Aprobado
+        else if (calificacionAlumno >= 60 && calificacionAlumno <= 89) {
+            System.out.println("El alumno esta aprobado");
+        } 
+        // No aprobado (Reprobado)
+        else if (calificacionAlumno >= 0 && calificacionAlumno <= 59) {
+            System.out.println("El alumno esta reprobado");
+        } 
+        else {
+            System.out.println("Calificacion no valida");   // 101
+        }
         // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al menos 15 años o ir acompañado.
 
         // 9. Crea un programa que diga si una letra es vocal o consonante.
