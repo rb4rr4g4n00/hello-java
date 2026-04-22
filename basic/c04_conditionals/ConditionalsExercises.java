0a581cb5127ed23f1e90ba4405e0af72d17134ac
CARLITOX2000 <barragancarlosjesus@gmail.com>
2026-04-22 10:16:46 -0600

    Se solucionaron los problemas faltantes

--- basic/c04_conditionals/ConditionalsExercises.java
+++ basic/c04_conditionals/ConditionalsExercises.java
@@ -42,10 +42,42 @@ public class ConditionalsExercises {
             System.out.println("Es impar");
         }
         // 5. Verifica si un número está en el rango de 1 a 100.
-
+        int valor = 50;
+        if (valor >= 1 (( valor <= 100))) {
+            System.out.println("Esta en el rango");
+        }else {
+            System.out.println("Esta fuera del rango");
+        }
         // 6. Declara una variable con el día de la semana (1-7) y muestra su nombre con switch.
+        int dia = 3;
 
+        switch (dia) {
+            case 1:
+                System.out.println("Lunes");
+                break;
+            case 2:
+                System.out.println("Martes");
+                break;
+            case 3:
+                System.out.println("Miércoles");
+                break;
+            case 4:
+                System.out.println("Jueves");
+                break;
+            case 5:
+                System.out.println("Viernes");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Día inválido");
+        }
         // 7. Simula un sistema de notas: muestra "Sobresaliente", "Aprobado" o "Suspenso" según la nota (0-100).
+        int calificacionAlumno = 85;
         // Sobresaliente
         if (calificacionAlumno >= 90 && calificacionAlumno <= 100) {
             System.out.println("El alumno es sobresaliente");
@@ -62,10 +94,30 @@ public class ConditionalsExercises {
             System.out.println("Calificacion no valida");   // 101
         }
         // 8. Escribe un programa que determine si puedes entrar al cine: debes tener al menos 15 años o ir acompañado.
-
+        int edadCine = 14;
+        boolean acompanado  = true; 
+        if (edadCine >= 15 || acompanado) {
+            System.out.println("Puede entrar al cine");
+        } else { 
+            System.out.println("No puede entar al cien");
+        }
         // 9. Crea un programa que diga si una letra es vocal o consonante.
-
+        char letra = 'a';
+        if (letra == 'a' || letra == 'e' || letra == 'i' || letra =='o' || letra == 'u') {
+            System.out.println("Es vocal");
+        } else { 
+            System.out.println("Es constante");
+        }
         // 10. Usa tres variables a, b, c y muestra cuál es el mayor de las tres.
-
+        int a = 5;
+        int b = 9;
+        int c = 3;
+        if (a >= b && a >= c) {
+            System.out.println("El mayor es: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("El mayor es: " + b);
+        } else {
+            System.out.println("El mayor es: " + c);
+        }
     }
 }
