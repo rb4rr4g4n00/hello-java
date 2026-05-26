f59806b94311a4edca3e6b34ed8b4f5494a30b79
CARLITOX2000 <barragancarlosjesus@gmail.com>
2026-05-25 19:11:34 -0600

    Guardar cambios

--- basic/c05_structures/StructuresExercises.java
+++ basic/c05_structures/StructuresExercises.java
@@ -1,4 +1,6 @@
-package basic.c05_structures;
+
+
+import java.util.ArrayList;
 
 /*
 Clase 44 - Ejercicios: Estructuras
@@ -10,13 +12,23 @@ public class StructuresExercises {
     public static void main(String[] args) {
 
         // 1. Crea un Array con 5 valores e imprime su longitud.
-
+        int[] numeros = {10, 20, 30, 40, 50};
+        System.out.println("Longitud del array: " + numeros.length);
         // 2. Modifica uno de los valores del Array e imprime el valor del índice antes y después de modificarlo.
-
+        System.out.println("Valor antes de modificar: " + numeros[2]);
+        numeros[2] = 99;
+        System.out.println("Valor después de modificar: " + numeros[2]);
         // 3. Crea un ArrayList vacío.
-
+        ArrayList<String> nombres = new ArrayList<>()
         // 4. Añade 4 valores al ArrayList y elimina uno a continuación.
-
+        nombres.add("Carlos");
+        nombres.add("Ana");
+        nombres.add("Luis");
+        nombres.add("María");
+        System.out.println("ArrayList original: " + nombres);
+        nombres.remove("Luis");
+        System.out.println("ArrayList después de eliminar: " + nombres);
+    }
         // 5. Crea un HashSet con 2 valores diferentes.
 
         // 6. Añade un nuevo valor repetido y otro sin repetir al HashSet.
